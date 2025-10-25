adfb1354d068fef072ad43a81842cddbb8c72d22
Muhammad Tahir <mtahirkaam@gmail.com>
2025-10-25 20:31:31 +0500

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,21 @@
 import React from 'react'
-import lucide from 'lucide-react'
-
 const Home = () => {
   return (
-    <div>
-      <div>
-        <img
-            src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
-            alt="Uber Logo"
-            className="h-6 md:h-8"
-          />
-          <div>
-            <h2> Get Started with Uber</h2>
-              <button >
-                Continue
-              </button>
-            
-          </div>
+    <div className="bg-[url(../public/uber-bg.jpg)] h-screen w-full bg-gray-200 relative">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt="Uber Logo"
+        className="absolute top-4 left-4 h-6 md:h-8"
+      />
+
+      <div className="absolute bottom-8 left-0 right-0 flex justify-center">
+        <div className="flex flex-col gap-4 items-center bg-white p-8 rounded-lg shadow-lg">
+          <h2 className="text-lg font-semibold">Get Started With Uber</h2>
+          <button className="bg-black text-white px-6 py-2 rounded">Continue</button>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
